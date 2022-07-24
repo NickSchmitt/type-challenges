1e3ded8a90bfaa7f184afeffddd12849da36b910
nickschmitt <nickschmitt@gmail.com>
2022-07-24 09:01:50 -0700

    00043-easy-exclude

--- questions/00043-easy-exclude/test-cases.ts
+++ questions/00043-easy-exclude/test-cases.ts
@@ -1,5 +1,8 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+// string union T extends the more narrow literal U
+type MyExclude<T, U> = T extends U ? never : T
+
 type cases = [
   Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a'>, 'b' | 'c'>>,
   Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a' | 'b'>, 'c'>>,
