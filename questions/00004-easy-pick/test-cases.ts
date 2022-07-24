dd5e9b7e0f15a9ea13663272265bc11b1b365399
nickschmitt <nickschmitt@gmail.com>
2022-07-24 00:46:08 -0700

    00004-easy-pick — Implement Pick utility type

--- questions/00004-easy-pick/test-cases.ts
+++ questions/00004-easy-pick/test-cases.ts
@@ -1,5 +1,14 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+/**
+ * Can't do [P in K]: T[K]
+ * Because then we the return type for every prop would be string | boolean
+ * since we're saying T[K] could be any of the union in K, we need to narrow it to P, which is an
+ * individual property
+ * as opposed to either string or boolean depending on the key
+ */
+type MyPick<T, K extends keyof T> = { [P in K]: T[P] }
+
 type cases = [
   Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
   Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
