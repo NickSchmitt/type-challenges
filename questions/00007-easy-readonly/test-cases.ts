343bc2bcbf2e8495a5ebd4d29982a7b765450e14
nickschmitt <nickschmitt@gmail.com>
2022-07-24 00:49:29 -0700

    00007-easy-readonly — Implement Readonly builtin utility type

--- questions/00007-easy-readonly/test-cases.ts
+++ questions/00007-easy-readonly/test-cases.ts
@@ -1,5 +1,7 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type MyReadonly<T> = { readonly [P in keyof T]: T[P] }
+
 type cases = [
   Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
 ]
