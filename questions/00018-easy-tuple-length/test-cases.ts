a74ce64c58a694231ee15362da5ffec4c8e968b9
nickschmitt <nickschmitt@gmail.com>
2022-07-24 07:56:09 -0700

    00018-easy-tuple-length

--- questions/00018-easy-tuple-length/test-cases.ts
+++ questions/00018-easy-tuple-length/test-cases.ts
@@ -1,7 +1,11 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type Length<T extends Readonly<any[]>> = T['length']
+
 const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
 const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+
+let x: Length<typeof tesla>
 
 type cases = [
   Expect<Equal<Length<typeof tesla>, 4>>,
